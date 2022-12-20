fec508cb7854adcc7538d3decfdd6fdb1ce83d17
OsTa20 <90045622+OsTa20@users.noreply.github.com>
2022-12-20 23:41:26 +0300

    Update studentdata.js

--- js/studentdata.js
+++ js/studentdata.js
@@ -1,24 +1,24 @@
 let studentdata = [
       {
-        "name" : "أسامة",
+        "name" : "أسامة تقوى",
         "password" : "123",
         "id" : 1,
         "imgsrc" : "img/student/p1.png"
       },
       {
-        "name" : "حسن",
+        "name" : "حسن تقوى",
         "password" : "234",
         "id" : 2,
         "imgsrc" : "img/student/p2.jpg"
       },
       {
-        "name" : "ماهر",
+        "name" : "عبد المجيد العبد الله",
         "password" : "345",
         "id" : 3,
         "imgsrc" : "img/student/p3.jpg"
       },
       {
-        "name" : "مالك",
+        "name" : "مالك تقوى",
         "password" : "456",
         "id" : 4,
         "imgsrc" : "img/student/p4.jpg"
