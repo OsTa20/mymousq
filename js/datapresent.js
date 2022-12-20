65768e06d5c67425fdf3c8933786eb968a43219e
OsTa20 <90045622+OsTa20@users.noreply.github.com>
2022-12-20 23:39:27 +0300

    Update datapresent.js

--- js/datapresent.js
+++ js/datapresent.js
@@ -3,4 +3,6 @@ let datepresent = [
       ['saterday 2/12/2022',[1,2]],
       ['saterday 3/12/2022',[1]],
       ['saterday 4/12/2022',[2]],
+      ['saterday 5/12/2022',[2,3]],
+      ['saterday 6/12/2022',[3]]
   ];
