2cd1b77a8966791d48094c8de88934ef856f0d66
Osama Takwa <90045622+OsTa20@users.noreply.github.com>
2023-01-04 14:29:19 +0300

    Update studentdata.js

--- js/studentdata.js
+++ js/studentdata.js
@@ -3,7 +3,7 @@ let studentdata = [
         "name" : "أسامة تقوى",
         "password" : "123",
         "id" : 1,
-        "imgsrc" : "https://drive.google.com/file/d/15eRIlANNGrB-wOX76WgHNKSBsIF2QqJf/view?usp=drivesdk"
+        "imgsrc" : "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNY7waogSV_nk_YktSO3H8GR2BRMU1M0RjhsaZrV6Hx9kVz_0wB5XVYLXdH19G6Dbwor-Gg0t6LHUwICnfwspR2TSuyuLaMXgw9ViHS8jmcwmK0ojf19IV3WTvOq-XkgJoy3-O9ezk9UsnqfKRckx48__ZzrCI82WcQaVixf2GZ79P_FotdX456A5B/s500/%D8%AA%D8%B5%D9%85%D9%8A%D9%85%20%D8%A8%D8%AF%D9%88%D9%86%20%D8%B9%D9%86%D9%88%D8%A7%D9%86.png"
       },
       {
         "name" : "حسن تقوى",
