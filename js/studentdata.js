ca86a424cf54013379cabbd0a644ff9750260131
Osama Takwa <90045622+OsTa20@users.noreply.github.com>
2023-01-05 11:55:03 +0300

    Update studentdata.js

--- js/studentdata.js
+++ js/studentdata.js
@@ -3,25 +3,25 @@ let studentdata = [
         "name" : "أسامة تقوى",
         "password" : "123",
         "id" : 1,
-        "imgsrc" : "https://flic.kr/p/2o9PZrg"
+        "imgsrc" : "https://f.top4top.io/p_2561ytbqz1.png"
       },
       {
         "name" : "حسن تقوى",
         "password" : "234",
         "id" : 2,
-        "imgsrc" : "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNY7waogSV_nk_YktSO3H8GR2BRMU1M0RjhsaZrV6Hx9kVz_0wB5XVYLXdH19G6Dbwor-Gg0t6LHUwICnfwspR2TSuyuLaMXgw9ViHS8jmcwmK0ojf19IV3WTvOq-XkgJoy3-O9ezk9UsnqfKRckx48__ZzrCI82WcQaVixf2GZ79P_FotdX456A5B/s500/%D8%AA%D8%B5%D9%85%D9%8A%D9%85%20%D8%A8%D8%AF%D9%88%D9%86%20%D8%B9%D9%86%D9%88%D8%A7%D9%86.png"
+        "imgsrc" : "https://i.top4top.io/p_2561clvzg1.png"
       },
       {
         "name" : "عبد المجيد العبد الله",
         "password" : "345",
         "id" : 3,
-        "imgsrc" : "https://t.me/al_hedaya_2001/60.jpg"
+        "imgsrc" : "https://f.top4top.io/p_2561ytbqz1.png"
       },
       {
         "name" : "مالك تقوى",
         "password" : "456",
         "id" : 4,
-        "imgsrc" : "https://g.top4top.io/p_256198yf90.png"
+        "imgsrc" : "https://i.top4top.io/p_2561clvzg1.png"
       },
   ];
 // img/student/p4.jpg
